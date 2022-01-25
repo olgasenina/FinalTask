16eff02ff9f774fbb80e8b633e810f09f87b3267
olgasenina <o.v.senina2020@gmail.com>
2022-01-25 17:28:01 +0300

    Добавьте файлы проекта.

---
+++ FinalTask/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+namespace FinalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fPath = "C:/_МОЯ/Студенты/Students.dat";
+
+            DeserializeList(fPath);
+
+            Console.WriteLine("Для завершения работы программы нажмите любую клавишу");
+            Console.ReadKey();
+        }
+
+
+        static void DeserializeList(string fPath)
+        {
+            var students = new Student[] { };
+
+            // Вытащить список студентов из файла
+
+            using (FileStream fs = new FileStream(fPath, FileMode.Open))
+            {
+                try
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+
+                    students = (Student[])formatter.Deserialize(fs);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Не удалось выполнить десериализацию. Причина: " + e.Message);
+                }
+            }
+
+            // Создать на рабочем столе директорию Students.
+
+            string newPath = @"/Users/Ольга/Desktop/Students";
+
+            DirectoryInfo dirInfo = new DirectoryInfo(newPath);
+
+            try
+            {
+                if (dirInfo.Exists)
+                {
+                    dirInfo.Delete(true); // Удаление со всем содержимым
+                }
+
+                dirInfo.Create(); // Создать папку
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при создании каталога Students: {ex.Message}");
+            }
+
+            // Раскидать всех студентов из файла по группам (каждая группа - отдельный текстовый файл),
+            // в файле группы студенты перечислены построчно в формате "Имя, дата рождения".
+
+            foreach (Student aStudent in students)
+            {
+                try
+                {
+                    var fileInfo = new FileInfo(newPath + "/" + aStudent.Group + ".txt");
+
+                    if (fileInfo.Exists)
+                    {
+                        using (StreamWriter sw = fileInfo.AppendText()) // Добавляем запись
+                        {
+                            sw.WriteLine($"{aStudent.Name}, {aStudent.DateOfBirth.ToString("dd.MM.yyyy")}");
+                        }
+                    }
+                    else
+                    {
+                        using (StreamWriter sw = fileInfo.CreateText()) // Создаем файл
+                        {
+                            sw.WriteLine($"{aStudent.Name}, {aStudent.DateOfBirth.ToString("dd.MM.yyyy")}");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка при записи данных студента: {ex.Message}");
+                }
+
+            }
+
+        }
+    }
+}
---
+++ FinalTask/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+
+namespace FinalTask
+{
+    [Serializable] //   Атрибут сериализации
+    class Student
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        // Метод - конструктор
+        public Student(string name, string group, DateTime dateOfBirth)
+        {
+            Name = name;
+            Group = group;
+            DateOfBirth = dateOfBirth;
+        }
+    }
+}
